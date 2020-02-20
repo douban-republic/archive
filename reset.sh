5705bcbb45f4f73d6e0254cc2bbc8b729f61b0e3
akira <akira@douban.com>
2020-02-20 15:33:24 +0800

    test relative public path

--- reset.sh
+++ reset.sh
@@ -2,7 +2,7 @@
 git init
 git config user.name "akira"
 git config user.email "akira@douban.com"
-git remote add git@me.github.com:douban-republic/douban-republic.github.io.git
-#git add .
-#git commit -m 'fake initial commit'
-#git push -u origin master
+git remote add git@me.github.com:douban-republic/archive.git
+git add .
+git commit -m 'fake initial commit'
+git push -u origin master
