ce9ade15145347977076c03cb8adc86f274ca2c1
akira <akira@douban.com>
2020-02-22 16:42:00 +0800

    update read me

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,5 +5,6 @@
     <p>请不要频繁使用该功能。
       整个备份功能建立在 <a href="https://github.com/zce" target="_blank">zce</a> 同学架设的豆瓣API代理服务上，API请求数有限制。</p>
     <p>豆瓣API代理详情请见 <a href="https://douban.uieee.com/" target="_blank">zce的网站。</a></p>
+    <p>有任何问题请移步 Github issues，<a href="https://github.com/douban-republic/archive" target="_blank">仓库地址</a>。</p>
   </div>
 </template>
